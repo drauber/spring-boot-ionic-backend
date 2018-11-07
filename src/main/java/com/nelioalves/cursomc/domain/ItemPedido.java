32175dfea0609f581c83b8b8765b9ac7a8a43e6e
drauber <douglas.rauber@gmail.com>
2018-11-07 14:59:06 -0200

    subtotal e total do pedido

--- src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
+++ src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
@@ -31,6 +31,11 @@ public class ItemPedido implements Serializable {
 		this.preco = preco;
 	}
 
+	public Double getSubTotal() {
+		return (preco - desconto) * quantidade;
+	}
+	
+	
 	public ItemPedidoPK getId() {
 		return id;
 	}
