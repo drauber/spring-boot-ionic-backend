41628605a8fa5fcf2cc06b0963de575b89e71f0a
drauber <douglas.rauber@gmail.com>
2018-11-07 16:31:21 -0200

    Criando profile dev

--- src/main/java/com/nelioalves/cursomc/services/DBService.java
+++ src/main/java/com/nelioalves/cursomc/services/DBService.java
@@ -75,6 +75,7 @@ public class DBService {
 		Categoria cat10 = new Categoria(null, "Calça");
 		Categoria cat11 = new Categoria(null, "Perifericos");
 		Categoria cat12 = new Categoria(null, "Peças");
+		Categoria cat13 = new Categoria(null, "Ferramentas Cutelaria");
 		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -86,7 +87,7 @@ public class DBService {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 				
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8, cat9, cat10, cat11, cat12));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8, cat9, cat10, cat11, cat12, cat13));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 		
